7352d631402590f3245336d8e7df3476b8ec6c0d
Kelly Silva <kellynha756@gmail.com>
2023-06-21 12:44:19 +0300

    add high score and play button on main menu scene

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -7,13 +7,25 @@ public class ScoreSystem : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier = 10f;
-    
+
+    public const string HighScoreKey = "HighScore";
+
     private float score;
     
     void Update()
     {
         score += Time.deltaTime * scoreMultiplier;
 
-        scoreText.text = "Score: " + Mathf.FloorToInt(score).ToString();
+        scoreText.text = $"Score: {Mathf.FloorToInt(score)}";
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
